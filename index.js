05e59c5c07b6fe6562e79de973438aad921136a4
SOPHIA TEIXEIRA RAMADA <sophia.tramada@senacsp.edu.br>
2025-08-27 15:53:28 -0300

    aul 27.08

--- index.js
+++ index.js
@@ -3,11 +3,20 @@ var express = require("express");
 var app = express();
 
 var bodyParser = require('body-parser');
+
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
+
+// conexão no banco de dados
+conexao.connect(function(error){
+    if(error){
+        console.error("Erro ao conectar ao banco de dados: ", error);
+        process.exit(); // encerrar o servidor caso a conexão falhe
+    }
+});  
 
 app.get('/', function(req,res){
     res.sendFile(__dirname + '/cadastro.html');
@@ -18,30 +27,27 @@ app.post('/', function(req, res){
     var email = req.body.email;
     var senha = req.body.senha;
 
-    conexao.connect(function(error){
-    if(error) throw error;
+
 
     // prevenindo SQL injectiom
     var sql = "INSERT INTO estudante(nomecompleto, email, senha) VALUES (?, ?, ?)";
     conexao.query(sql, [nomecompleto, email, senha], function(error, result){
         if(error) throw error;
-        res.send("Estudante cadastrado com sucesso! " + result.insertId);
+        // res.send("Estudante cadastrado com sucesso! " + result.insertId);
+        res.redirect('/estudantes');
     });
     });
-});
 
 // Leitura do banco de dados
-app.get('/estudante', function(req, res){
-    conexao.connect(function(error){
-        if(error) console.log(error);
+app.get('/estudantes', function(req, res){
 
         var sql = "SELECT * FROM estudante";
         conexao.query(sql, function(error, result){
             if(error) console.log(error);
-            console.log(result);
+            // console.log(result);
+            res.render(__dirname + "/estudantes", {estudante:result});
         });
     });
-});
 
 app.listen(7000);
 
