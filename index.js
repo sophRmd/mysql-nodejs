2a1e69a4631da30a70bb51cce40ce1a14d1ab70c
SOPHIA TEIXEIRA RAMADA <sophia.tramada@senacsp.edu.br>
2025-09-03 16:27:18 -0300

    Aula 03.09

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 var conexao = require("./conexaobanco");
 var express = require("express");
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 var app = express();
 
-var bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(methodOverride('_method'));
+
 app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
 
 // conexão no banco de dados
 conexao.connect(function(error){
@@ -45,46 +49,70 @@ app.get('/estudantes', function(req, res){
         conexao.query(sql, function(error, result){
             if(error) console.log(error);
             // console.log(result);
-            res.render(__dirname + "/estudantes", {estudante:result});
+            res.render('estudantes', {estudante:result});
         });
     });
 
 // rota de delete
-app.get('/delete-estudante', function(req, res){
-    var sql = "DELETE FROM estudante WHERE id=?";
-    var id = req.query.id;
-    conexao.query(sql, [id], function(error, result){
-        if(error) console.log(error);
+app.delete('/delete-estudante', function(req, res){
+    // var sql = "DELETE FROM estudante WHERE id=?";
+    // var id = req.query.id;
+    // conexao.query(sql, [id], function(error, result){
+    //     if(error) console.log(error);
 
+    //     res.redirect('/estudantes');  -- usando esse, tem q usar app.get
+
+    // });
+
+    const id = req.body.id;
+    conexao.query('DELETE FROM estudante WHERE id = ?', [id], (err) =>{
+        if(err){
+            console.error(err);
+            return res.status(500).send('Erro ao deletar estudante');
+        }
         res.redirect('/estudantes');
-
     });
 });
 
 
 // Rota update
 app.get('/update-estudante', function(req, res){
-    var sql = "SELECT * FROM estudante WHERE id=?";
-    var id = req.query.id;
-    conexao.query(sql, [id], function(error, result){
-        if(error) console.log(error);
-        res.render(__dirname + '/alterarestudantes', {estudante:result});
+    // var sql = "SELECT * FROM estudante WHERE id=?";
+    // var id = req.query.id;
+    // conexao.query(sql, [id], function(error, result){
+    //     if(error) console.log(error);
+    //     res.render('alterarestudantes', {estudante:result});
+    // });
+    const id = req.query.id;
+
+    conexao.query("SELECT * FROM estudante WHERE id = ?", [id], (err, results) =>{
+        if(err) return res.status(500).send('Erro ao buscar estudante');
+        res.render('alterarestudantes', {estudante: results[0]})
     });
+
 });
 
-app.post('/update-estudante', function(req, res){
-    var nomecompleto = req.body.nomecompleto;
-    var email = req.body.email;
-    var senha = req.body.senha;
-    var id = req.body.id;
+app.put('/update-estudante', function(req, res){
+    // var nomecompleto = req.body.nomecompleto;
+    // var email = req.body.email;
+    // var senha = req.body.senha;
+    // var id = req.body.id;
 
-    var sql = "UPDATE estudante SET nomecompleto = ?, email = ?, senha = ? WHERE id = ?";
-    var id = req.query.id;
-    conexao.query(sql, [nomecompleto, email, senha, id], function(error, result){
-        if(error) console.log(error);
-        res.render(__dirname + '/alterarestudantes', {estudante:result});
+    // var sql = "UPDATE estudante SET nomecompleto = ?, email = ?, senha = ? WHERE id = ?";
+    // var id = req.query.id;
+    // conexao.query(sql, [nomecompleto, email, senha, id], function(error, result){
+    //     if(error) console.log(error);
+    //     res.render(__dirname + '/alterarestudantes', {estudante:result});
+    //     res.redirect('/estudantes'); -- para funcionar, tem q usar app.post
+    // });
+
+    const{id, nomecompleto, email, senha} = req.body;
+
+    conexao.query('UPDATE estudante SET nomecompleto=?, email=?, senha=? WHERE id=?', [nomecompleto, email, senha, id], (err) => {
+        if(err) return res.status(500).send('Erro ao atualizar estudante');
         res.redirect('/estudantes');
     });
+
 });
 
 app.listen(7000);
